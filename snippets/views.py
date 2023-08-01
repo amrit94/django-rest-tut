5e92920ad8bb2d1a625eb346bcb7dfbdeaf9b1e3
Amrit <amritpra94@gmail.com>
2023-08-01 22:49:32 +0530

    gitignore and tes-view added

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(requests):
+    html = "<html><body><h1>Welcome</h1></body></html>"
+    return HttpResponse(html)
