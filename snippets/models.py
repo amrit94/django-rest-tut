0e460aff62473a9cfaf56eeb3b21da5d157a910f
Amrit <amritpra94@gmail.com>
2023-08-01 23:32:18 +0530

    Model created and sample data added to db

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+STYLE_CHOICES = [('bild', 'Bold'), ('underline', 'Underline'), ('friendly', 'Friendly')]
+LANGUAGE_CHOICES = (('python', 'Python'), ('js', 'JavaScript'), ('java', 'Java'))
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm=^p6!45dqe(2gryqpxdxe(m(s%krr#0+z=q_u=5e#@jknxx4$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'snippets',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
